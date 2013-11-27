f376d022239623f3e4471b7df023915fc2a1690d
Krystian Kuczek <krystian@kuczek.eu>
2013-11-27 15:34:46 +0100

    Translate polish characters to standard one's.

--- src/Knp/DoctrineBehaviors/Model/Sluggable/Sluggable.php
+++ src/Knp/DoctrineBehaviors/Model/Sluggable/Sluggable.php
@@ -94,8 +94,10 @@ trait Sluggable
 
             // generate the slug itself
             $sluggableText = implode($usableValues, ' ');
-            
+
             //Here will go polish characters.
+            $sluggableText = strtr($sluggableText, "ążśźęćńółĄŻŚŹĘĆŃÓŁ" "azszecnolAZSZECNOL");
+            
             $urlized = strtolower( trim( preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', iconv('UTF-8', 'ASCII//TRANSLIT', $sluggableText) ), $this->getSlugDelimiter() ) );
             $urlized = preg_replace("/[\/_|+ -]+/", $this->getSlugDelimiter(), $urlized);
 
